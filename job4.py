20a2903b609395ec32a37703001317c2a2818aca
Alex1986ham <34843315+Alex1986ham@users.noreply.github.com>
2019-04-30 16:43:35 +0200

    Create job4.py

---
+++ job4.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+#searchfile = open('C:/Users/D&D/Downloads/Downloads2/1.txt')
+if os.path.isfile('C:/Users/D&D/Downloads/Downloads2/%.txt'):
+    print("file")
+else:
+    print("not")
+
+#if searchfile in '.txt':
+#    print("found")
+#else:
+#    print("not found")
